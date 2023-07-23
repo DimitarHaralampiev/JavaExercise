8e92498fd289727ea4764d84edacdd7c8216bc6e
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-07-23 21:37:03 +0300

    feat: Exercise ObjectsAndClasses

---
+++ src/ObjectsAndClasses/GroomingSalon/GroomingSalon.java
@@ -0,0 +1,60 @@
+package ObjectsAndClasses.GroomingSalon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroomingSalon {
+
+    private List<Pet> data;
+
+    private int capacity;
+
+    public GroomingSalon(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Pet pet){
+
+        if (this.capacity > data.size()){
+            this.data.add(pet);
+        }
+    }
+
+    public boolean remove(String name){
+
+        for (Pet pet : this.data){
+            if (pet.getName().equals(name)){
+                this.data.remove(pet);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Pet getPet(String name, String owner){
+
+        for (Pet pet : this.data){
+            if (pet.getName().equals(name)){
+                return new Pet(name, pet.getAge(), owner);
+            }
+
+        }
+        return null;
+    }
+
+    public int getCount(){
+        return this.data.size();
+    }
+
+    public String getStatistics(){
+        System.out.println(" The grooming salon has the following clients:");
+
+        String str = " ";
+        for (Pet pet : this.data){
+            str += (getPet(pet.getName(), pet.getOwner())) + "\n";
+        }
+
+        return str;
+    }
+}
---
+++ src/ObjectsAndClasses/GroomingSalon/Pet.java
@@ -0,0 +1,47 @@
+package ObjectsAndClasses.GroomingSalon;
+
+public class Pet {
+
+    private String name;
+
+    private int age;
+
+    private String owner;
+
+    public Pet(String name, int age, String owner) {
+        this.name = name;
+        this.setAge(age);
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.age + " - " + this.owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
