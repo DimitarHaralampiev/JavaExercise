ba85dcc0c94545a621651fdc21520e3ee98896a1
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-07-27 19:45:33 +0300

    feat: Add Exercise Enum

---
+++ src/OOP/Abstraction/CardRank/Main.java
@@ -0,0 +1,19 @@
+package OOP.Abstraction.CardRank;
+
+import java.util.Scanner;
+
+public class Main {
+
+    enum CardRank { ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        System.out.println(text + ":");
+        for (CardRank card : CardRank.values()){
+            System.out.println("Ordinal values: " + card.ordinal() + "; " + "Name value: " + card);
+        }
+    }
+}
